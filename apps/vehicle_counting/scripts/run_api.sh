f276d11635417545f799f3a6d545f52823098abd
Loc Huynh <lohuynh@microsoft.com>
2022-04-12 17:22:19 -0700

    update run_api.sh script to clean up completed jobs every 60 secs

--- apps/vehicle_counting/scripts/run_api.sh
+++ apps/vehicle_counting/scripts/run_api.sh
@@ -5,4 +5,10 @@ cp -f /mnt/secrets-store/AksKubeConfig /root/.kube/config
 kubectl get pods
 
 export PYTHONPATH=/app
-python3 apps/vehicle_counting/api/app.py
+python3 apps/vehicle_counting/api/app.py &
+
+while true
+do
+	kubectl delete job $(kubectl get job -n nxsapp -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}') -n nxsapp
+	sleep 60
+done
